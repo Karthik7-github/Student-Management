be0cbf11e46ceab452d2f1504a6c8e993ff8b5df
KARTHIK <ponnamk19@gmail.com>
2026-05-19 17:12:58 +0530

    adding new things

--- Frontend/src/pages/Registrations/teacherres.jsx
+++ Frontend/src/pages/Registrations/teacherres.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 
 const teacherres = () => {
-  return (
-    <div>teacherres</div>
-  )
+   return (
+    <div>
+      <div className="stuform teach">
+        <h1>Lecturer (Adimin) Registraion Form</h1>
+        <div className="stubox">
+          <form action="">
+            <div className="forml1">
+            <div class="inputBox">
+            <input required="" type="text" placeholder="Write here..." required/>
+              <span>Name :</span>
+            </div>
+             <div class="inputBox">
+              <input required="" type="text" placeholder="Write here..." required/>
+              <span>Age :</span>
+            </div>
+             <div class="inputBox">
+              <input required="" type="text" placeholder="Write here..." required/>
+              <span>DOB :</span>
+            </div>
+          </div>
+          <div className="forml1">
+            <div class="inputBox">
+              <input required="" type="text" placeholder="Write here..." required/>
+              <span>Subject :</span>
+            </div>
+             <div class="inputBox">
+              <input required="" type="text" placeholder="Write here as Student ..." required/>
+              <span>Role :</span>
+            </div>
+             <div class="inputBox">
+              <input required="" type="text" placeholder="Write here..." required/>
+              <span>Email :</span>
+            </div>
+          </div>
+          <div className="forml1">
+             <div class="inputBox">
+              <input required="" type="password" placeholder="Write here..." required/>
+              <span>Password :</span>
+            </div>
+            <button>
+              <span class="shadow"></span>
+              <span class="edge"></span>
+              <span class="front text" type="submit"> Submit
+              </span>
+            </button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default teacherres
