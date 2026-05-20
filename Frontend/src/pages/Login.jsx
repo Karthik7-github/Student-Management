b86fbdd0a3a0f133ac34599fb58d0da3c3d274e9
KARTHIK <ponnamk19@gmail.com>
2026-05-20 11:23:22 +0530

    adding security

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,30 +3,34 @@ import React from "react";
 const Login = () => {
   return (
     <div>
+      <form>
       <div className="Loginpage">
         <div className="Loginbox">
           <h1>Login</h1>
           <div class="brutalist-container styleinput">
             <input
-              placeholder="TYPE HERE"
+              placeholder="TYPE EMAIL..."
               class="brutalist-input smooth-type"
-              type="text"
+              type="email"
+              required
             />
-            <label class="brutalist-label">Email / Mobile No :</label>
+            <label class="brutalist-label">Email : </label>
           </div>
           <div class="brutalist-container styleinput">
             <input
-              placeholder="TYPE HERE"
+              placeholder="TYPE PASSWORD..."
               class="brutalist-input smooth-type"
-              type="text"
+              type="password"
+              required
             />
             <label class="brutalist-label">Password : </label>
           </div>
-          <button class="button">
+          <button class="button" type="submit">
             <span>Submit</span>
           </button>
         </div>
       </div>
+      </form>
     </div>
   );
 };
